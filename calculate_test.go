f297fa559baf2cb1cca3d34acd2dc07b017556ff
riyan-eng <fbriyant11@gmail.com>
2023-01-06 15:12:47 +0700

    update to sub test

--- calculate_test.go
+++ calculate_test.go
@@ -13,23 +13,51 @@ type Testcase struct {
 }
 
 func TestCalculateIsArmstrong(t *testing.T) {
-	testCase := Testcase{
-		value:    371,
-		expected: true,
-	}
-	testCase.actual = golangunittesting.CalculateIsArmstrong(testCase.value)
-	if testCase.expected != testCase.actual {
-		t.Fail()
-	}
+	t.Run("should pass for case 371", func(t *testing.T) {
+		testCase := Testcase{
+			value:    371,
+			expected: true,
+		}
+		testCase.actual = golangunittesting.CalculateIsArmstrong(testCase.value)
+		if testCase.expected != testCase.actual {
+			t.Fail()
+		}
+
+	})
+	t.Run("should pass for case 370", func(t *testing.T) {
+		testCase := Testcase{
+			value:    370,
+			expected: true,
+		}
+		testCase.actual = golangunittesting.CalculateIsArmstrong(testCase.value)
+		if testCase.expected != testCase.actual {
+			t.Fail()
+		}
+
+	})
 }
 
 func TestNegativeCalculateIsArmstrong(t *testing.T) {
-	testCase := Testcase{
-		value:    350,
-		expected: false,
-	}
-	testCase.actual = golangunittesting.CalculateIsArmstrong(testCase.value)
-	if testCase.expected != testCase.actual {
-		t.Fail()
-	}
+	t.Run("should fail for case 350", func(t *testing.T) {
+		testCase := Testcase{
+			value:    350,
+			expected: false,
+		}
+		testCase.actual = golangunittesting.CalculateIsArmstrong(testCase.value)
+		if testCase.expected != testCase.actual {
+			t.Fail()
+		}
+
+	})
+	t.Run("should fail for case 300", func(t *testing.T) {
+		testCase := Testcase{
+			value:    300,
+			expected: false,
+		}
+		testCase.actual = golangunittesting.CalculateIsArmstrong(testCase.value)
+		if testCase.expected != testCase.actual {
+			t.Fail()
+		}
+
+	})
 }
